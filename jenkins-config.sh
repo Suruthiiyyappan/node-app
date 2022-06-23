d7fd1d1112452186d3e16b6c49b49cce2c7feaff
Suruthi <96717233+Suruthiiyyappan@users.noreply.github.com>
2022-06-23 22:53:43 +0530

    Update jenkins-config.sh

--- jenkins-config.sh
+++ jenkins-config.sh
@@ -1,14 +1,14 @@
-app_tag=`git ls-remote https://github.com/javahometech/node-app HEAD | awk '{print $1}'`
+app_tag=`git ls-remote https://github.com/Suruthiiyyappan/node-app HEAD | awk '{print $1}'`
 
-docker_app="kammana/nodejenkins:$app_tag"
+docker_app="642000/nodejenkins:$app_tag"
 docker build -t $docker_app .
 
-docker login -u kammana -p yourpassword 
+docker login -u 642000 -p yourpassword 
 
 docker push $docker_app
 
-scp -i /var/lib/jenkins/dev.pem deploy.sh ec2-user@172.31.43.90:/tmp
+scp -i /var/lib/jenkins/dev.pem deploy.sh ec2-user@172.31.91.130:/tmp
 
-ssh -i /var/lib/jenkins/dev.pem ec2-user@172.31.43.90 chmod +x /tmp/deploy.sh
+ssh -i /var/lib/jenkins/dev.pem ec2-user@172.31.91.130 chmod +x /tmp/deploy.sh
 
-ssh -i /var/lib/jenkins/dev.pem ec2-user@172.31.43.90 /tmp/deploy.sh $docker_app
+ssh -i /var/lib/jenkins/dev.pem ec2-user@172.31.91.130 /tmp/deploy.sh $docker_app
